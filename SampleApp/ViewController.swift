0bd38306f88877296139690d7770cec99d90be1d
hironobu-iga <hironobu.iga.developer@gmail.com>
2017-05-07 16:59:29 +0900

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by 伊賀裕展 on 2017/05/07.
+//  Copyright © 2017年 伊賀裕展. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
